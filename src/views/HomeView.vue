81bcb037caf1471daeff9385df7f2b3710adc8fe
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-08-10 20:04:37 -0600

    Se agrego el archivo HomeView.vue y el codigo que lo hara funcionar como pagina de inicio

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png">
+  <BlogHeader/>
+  <BlogFecha/>
+  <BlogEtiqueta/>
+  <BlogContenido/>
+  <BlogFooter/>
+</template>
+
+<script>
+import BlogHeader from "./components/BlogHeader.vue"
+import BlogFecha from "./components/BlogFecha.vue"
+import BlogEtiqueta from "./components/BlogEtiqueta.vue"
+import BlogContenido from "./components/BlogContenido.vue"
+import BlogFooter from "./components/BlogFooter.vue"
+export default {
+  name: 'HomeView',
+  components: {
+      BlogHeader,
+      BlogFecha,
+      BlogEtiqueta,
+      BlogContenido,
+      BlogFooter,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
