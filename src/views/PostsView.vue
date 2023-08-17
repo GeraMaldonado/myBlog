076443bb304baabc8eadbc06c7607becc572054c
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-08-16 19:18:47 -0600

    Se eliminó el header y footer, pues ya esta en app.vue

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,9 +1,5 @@
 <template>
     <div class="container">
-  <header>
-      <img alt="Vue logo" src="../assets/logo.png">
-      <BlogHeader/>
-  </header>
   <main>
       <section>
           <BlogContenido/>
@@ -15,30 +11,23 @@
       </aside>
   </main>
     </div>
-  <footer>
-      <BlogFooter/>
-  </footer>
 </template>
 
 <script>
-import BlogHeader from "../components/BlogHeader.vue"
 import BlogFecha from "../components/BlogFecha.vue"
 import BlogEtiqueta from "../components/BlogEtiqueta.vue"
 import BlogContenido from "../components/BlogContenido.vue"
-import BlogFooter from "../components/BlogFooter.vue"
 import PrimerPost from "../components/post/PrimerPost.vue"
 export default {
-  name: 'HomeView',
+  name: 'PostsView',
   components: {
-      BlogHeader,
       BlogFecha,
       BlogEtiqueta,
       BlogContenido,
-      BlogFooter,
       PrimerPost,
   }
 }
 </script>
 
-<style src="../assets/styles/HomeView.css">
+<style src="../assets/styles/PostsView.css">
 </style>
