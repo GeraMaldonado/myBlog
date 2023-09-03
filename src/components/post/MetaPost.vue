01430ceffc8827c917dc5664abc98e11ea456703
GeraMaldonado <99371080+GeraMaldonado@users.noreply.github.com>
2023-09-02 20:12:44 -0600

    Actualizar MetaPost.vue
    agregando contenido al post

---
+++ src/components/post/MetaPost.vue
@@ -0,0 +1,3 @@
+<template>
+<p>Para poder comenzar otro proyecto, me gustaría dejar funcional el blog, esto pensando en que ya esté toda la base construida, y las modificaciones sean leves, para esto por último me gustaría dos cosas finales, la primera es sacar la función del tiempo para que requiera menos espacio en los post, la otra sería mejorar el archivo router/index.js, para evitar crear tantas líneas por los post, este es el que aún no he planeado como hacerlo, aún tengo que investigar. Con estos últimos cambios ya podría comenzar otros proyectos sin sentir culpa o que abandoné el blog, o podría comenzar otros post que no tengan que ver con el blog<p>
+</template>
