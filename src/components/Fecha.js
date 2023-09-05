75547aeb19e8a652bee930a1e928c9fc812b8a64
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-09-05 16:52:33 -0600

    Se factorizo parte del codigo para crear la fecha, para solo hacer un llamado a la funcion, e instanciarlo

---
+++ src/components/Fecha.js
@@ -0,0 +1,8 @@
+class Fecha{
+    static FechaCreacion(d, m, y){
+        const fecha = new Date(y,m,d);
+        const mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+        return `${fecha.getDate()} de ${mes[fecha.getMonth()]} del ${fecha.getFullYear()}`
+    }
+}
+module.exports = Fecha;
