d3be8e2adffb7ca63048177ca0ff560a7917bbbd
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-09-07 17:47:17 -0600

    Se cambia la entrada del mes para qu se represente correctamente

--- src/components/Fecha.js
+++ src/components/Fecha.js
@@ -1,6 +1,6 @@
 class Fecha{
     static FechaCreacion(d, m, y){
-        const fecha = new Date(y,m,d);
+        const fecha = new Date(y,m-1,d);
         const mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
         return `${fecha.getDate()} de ${mes[fecha.getMonth()]} del ${fecha.getFullYear()}`
     }
--- src/components/post/MetasBlog.vue
+++ src/components/post/MetasBlog.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="post">
         <h2>Metas blog</h2>
-        <h3>{{generarFecha(2, 8, 2023)}}</h3>
+        <h3>{{generarFecha(2, 9, 2023)}}</h3>
         <p>Para poder comenzar otro proyecto, me gustaría dejar funcional el blog, esto pensando en que ya esté toda la base construida, y las modificaciones sean leves, para esto por último me gustaría dos cosas finales, la primera es sacar la función del tiempo para que requiera menos espacio en los post, la otra sería mejorar el archivo router/index.js, para evitar crear tantas líneas por los post, este es el que aún no he planeado como hacerlo, aún tengo que investigar. Con estos últimos cambios ya podría comenzar otros proyectos sin sentir culpa o que abandoné el blog, o podría comenzar otros post que no tengan que ver con el blog</p>
     </div>
 </template>
