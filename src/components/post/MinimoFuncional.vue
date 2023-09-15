f7491c4596f906cbc768d4bc388e35750588698e
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-09-14 23:34:15 -0600

    Se creo un nuevo archivo para un nuevo post

---
+++ src/components/post/MinimoFuncional.vue
@@ -0,0 +1,3 @@
+<template>
+    <!--Solo necesito unos ultimos toques para que el blog tenga el minimo funcional para desplegarse, que es el creador de rutas de los posts, un nuevo router, y quiza eqiquetas, aunque eso por el momento lo veo inecesario, como un minimo funcional, pero ya esta a nada para poderlo desplegar y hacer una prueba -->
+</template>
