4d137ff7e5387d49b4fcacdb248f085d8ca1a2d2
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2023-09-18 23:19:30 -0600

    Corrigiendo errores y recompilando

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/myBlog/' : '/',
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/myBlog/'
+    : '/'
 }
 
